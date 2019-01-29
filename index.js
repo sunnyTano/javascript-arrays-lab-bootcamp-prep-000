f6851053e8b5a4407374277dc45211b301debc64
Susan Tan <susan.tan198@gmail.com>
2019-01-29 17:33:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var kittens = ["Milo", "Otis", "Garfield"]
 // Add your functions and code here
 
 function destructivelyAppendKitten(name){
-  kittens.push(el)
+  kittens.push(name)
   return kittens;
 }
 
